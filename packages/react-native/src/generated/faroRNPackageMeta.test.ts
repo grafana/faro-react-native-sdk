f2a7b5f50264bdffda9b2abfc5620740249f3a35
yduartep <layahidev82@gmail.com>
2026-04-15 13:04:08 +0200

    fix: rollback to alpha version

--- packages/react-native/src/generated/faroRNPackageMeta.test.ts
+++ packages/react-native/src/generated/faroRNPackageMeta.test.ts
@@ -3,7 +3,7 @@ import packageJson from '../../package.json';
 import { FARO_REACT_NATIVE_NPM_NAME, FARO_REACT_NATIVE_NPM_VERSION } from './faroRNPackageMeta';
 
 describe('faroRNPackageMeta', () => {
-  it('should match package.json (run yarn build:compile prep script after version bumps)', () => {
+  it('should match package.json (run yarn sync:package-meta after manual version edits, or use lerna version)', () => {
     expect(FARO_REACT_NATIVE_NPM_NAME).toBe(packageJson.name);
     expect(FARO_REACT_NATIVE_NPM_VERSION).toBe(packageJson.version);
   });
