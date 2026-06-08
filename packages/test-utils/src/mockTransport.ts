1c2fa3f43ca7a901327178a8d5c48009c799260b
aranhave <vishwan.aranha@grafana.com>
2026-06-08 15:45:51 -0400

    test: fix mock transport with updated core

--- packages/test-utils/src/mockTransport.ts
+++ packages/test-utils/src/mockTransport.ts
@@ -1,25 +1,58 @@
-import { BaseTransport, VERSION } from '@grafana/faro-core';
-import type { Patterns, Transport, TransportItem } from '@grafana/faro-core';
+import { defaultUnpatchedConsole, noop, VERSION } from '@grafana/faro-core';
+import type {
+  Config,
+  InternalLogger,
+  Metas,
+  Patterns,
+  Transport,
+  TransportItem,
+  UnpatchedConsole,
+} from '@grafana/faro-core';
 
-export class MockTransport extends BaseTransport implements Transport {
+export class MockTransport implements Transport {
   readonly name = '@grafana/transport-mock';
   readonly version = VERSION;
 
+  unpatchedConsole: UnpatchedConsole = defaultUnpatchedConsole;
+  internalLogger: InternalLogger = {
+    debug: noop,
+    error: noop,
+    info: noop,
+    prefix: '@grafana/transport-mock',
+    warn: noop,
+  };
+  config: Config = {} as Config;
+  metas: Metas = {} as Metas;
+
   items: TransportItem[] = [];
 
-  constructor(private ignoreURLs: Patterns = []) {
-    super();
+  constructor(private ignoreURLs: Patterns = []) {}
+
+  send(items: TransportItem | TransportItem[]): void | Promise<void> {
+    this.items.push(...(Array.isArray(items) ? items : [items]));
   }
 
-  send(items: TransportItem[]): void | Promise<void> {
-    this.items.push(...items);
-  }
-
-  override isBatched(): boolean {
+  isBatched(): boolean {
     return true;
   }
 
-  override getIgnoreUrls(): Patterns {
+  getIgnoreUrls(): Patterns {
     return this.ignoreURLs;
+  }
+
+  logDebug(...args: unknown[]): void {
+    this.internalLogger.debug(`${this.name}\n`, ...args);
+  }
+
+  logInfo(...args: unknown[]): void {
+    this.internalLogger.info(`${this.name}\n`, ...args);
+  }
+
+  logWarn(...args: unknown[]): void {
+    this.internalLogger.warn(`${this.name}\n`, ...args);
+  }
+
+  logError(...args: unknown[]): void {
+    this.internalLogger.error(`${this.name}\n`, ...args);
   }
 }
